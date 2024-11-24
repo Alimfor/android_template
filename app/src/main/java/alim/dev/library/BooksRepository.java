433e54dc61ac132568f2d1d84981146fef96843e
a.gaziev <a.gaziev@jgarant.kz>
2024-11-24 15:37:07 +0500

    feat: create a project that has several books with description

---
+++ app/src/main/java/alim/dev/library/BooksRepository.java
@@ -0,0 +1,13 @@
+package alim.dev.library;
+
+public class BooksRepository {
+    public static String[] getBooks() {
+        return new String[] {
+                "Книга 1: Краткое содержание",
+                "Книга 2: Краткое содержание",
+                "Книга 3: Краткое содержание",
+                "Книга 4: Краткое содержание",
+                "Книга 5: Краткое содержание"
+        };
+    }
+}
